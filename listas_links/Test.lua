b6beb987518555404b6164ac070210df08dabbaa
Mist the Goat <62227356+Mistextreme@users.noreply.github.com>
2025-11-29 22:44:02 +0000

    Upload da lista de links: Test.lua

---
+++ listas_links/Test.lua
@@ -0,0 +1,4 @@
+return {
+    ["edited_banner_a_cyberpunk_figure_wearing_a_neon_outlined_goat_ma"] = {label = "edited_banner_a_cyberpunk_figure_wearing_a_neon_outlined_goat_ma", image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/assets/images/Teste/edited_banner_a_cyberpunk_figure_wearing_a_neon_outlined_goat_ma.png"},
+    ["mist"] = {label = "mist", image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/assets/images/Teste/mist.png"}
+}
