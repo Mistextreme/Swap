e4c41586c7f337616aa897d499946a43ff7113a0
Mist the Goat <62227356+Mistextreme@users.noreply.github.com>
2025-11-30 19:00:47 +0000

    Upload da lista de links: Links_videos-apps.lua

---
+++ listas_links/Links_videos-apps.lua
@@ -0,0 +1,4 @@
+return {
+    ["fivemanage_tool_4"] = {label = "fivemanage_tool_4", video = "https://raw.githubusercontent.com/Mistextreme/Swap/main/assets/videos/Apps/fivemanage_tool_4.mp4"},
+    ["fivemanage_tool_3"] = {label = "fivemanage_tool_3", video = "https://raw.githubusercontent.com/Mistextreme/Swap/main/assets/videos/Apps/fivemanage_tool_3.mp4"}
+}
