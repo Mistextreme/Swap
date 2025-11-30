32694eb28dca298899d91cecdc76ce601324d0dc
Mist the Goat <62227356+Mistextreme@users.noreply.github.com>
2025-11-30 15:41:17 +0000

    Upload da lista de links: Links_videos.lua

---
+++ listas_links/Links_videos.lua
@@ -0,0 +1,4 @@
+return {
+    ["Mist--devv"] = {label = "Mist--devv", video = "https://raw.githubusercontent.com/Mistextreme/Swap/main/assets/videos/2025-11-30/Mist--devv.mp4"},
+    ["Mist__dev"] = {label = "Mist__dev", video = "https://raw.githubusercontent.com/Mistextreme/Swap/main/assets/videos/2025-11-30/Mist__dev.mp4"}
+}
