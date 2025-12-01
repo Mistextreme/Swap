8652abd719db524ced9140b5f8150f6518742725
Mist the Goat <62227356+Mistextreme@users.noreply.github.com>
2025-12-01 12:49:03 +0000

    Upload da lista de links: Grok_png.lua

---
+++ listas_links/Grok_png.lua
@@ -0,0 +1,9 @@
+return {
+    ["grok_video_2025-12-01-12-32-26"] = {label = "grok_video_2025-12-01-12-32-26", image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/assets/images/Grok_png/grok_video_2025-12-01-12-32-26.gif"},
+    ["grok_video_2025-12-01-12-34-41"] = {label = "grok_video_2025-12-01-12-34-41", image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/assets/images/Grok_png/grok_video_2025-12-01-12-34-41.gif"},
+    ["IMG_20251201_123312"] = {label = "IMG_20251201_123312", image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/assets/images/Grok_png/IMG_20251201_123312.jpg"},
+    ["IMG_20251201_123219"] = {label = "IMG_20251201_123219", image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/assets/images/Grok_png/IMG_20251201_123219.jpg"},
+    ["IMG_20251201_115932"] = {label = "IMG_20251201_115932", image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/assets/images/Grok_png/IMG_20251201_115932.jpg"},
+    ["2048px-Lua-Logo.svg"] = {label = "2048px-Lua-Logo.svg", image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/assets/images/Grok_png/2048px-Lua-Logo.svg.png"},
+    ["41Nq6vJMeVL._AC_UF1000,1000_QL80_"] = {label = "41Nq6vJMeVL._AC_UF1000,1000_QL80_", image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/assets/images/Grok_png/41Nq6vJMeVL._AC_UF1000%2C1000_QL80_.jpg"}
+}
