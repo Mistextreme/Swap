d699cf3a29071add22e350899c6546908093342b
Mist the Goat <62227356+Mistextreme@users.noreply.github.com>
2025-12-02 20:47:37 +0000

    Add files via upload

---
+++ listas_links/images/GitHub/github_images_images-new.lua
@@ -0,0 +1,347 @@
+-- GitHub Images
+return {
+    ["1762460899"] = {
+        label = "1762460899",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images_new/1762460899.png"
+    },
+    ["1762460945"] = {
+        label = "1762460945",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images_new/1762460945.png"
+    },
+    ["1764161633"] = {
+        label = "1764161633",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images_new/1764161633.png"
+    },
+    ["1764161761"] = {
+        label = "1764161761",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images_new/1764161761.png"
+    },
+    ["1764268003"] = {
+        label = "1764268003",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images_new/1764268003.png"
+    },
+    ["1764268327"] = {
+        label = "1764268327",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images_new/1764268327.png"
+    },
+    ["1764523041"] = {
+        label = "1764523041",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images_new/1764523041.png"
+    },
+    ["banner_banner_cover_for_a_fivem_security_application_call (1)"] = {
+        label = "banner_banner_cover_for_a_fivem_security_application_call (1)",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images_new/banner_banner_cover_for_a_fivem_security_application_call (1).jpg"
+    },
+    ["banner_banner_cover_for_a_fivem_security_application_call"] = {
+        label = "banner_banner_cover_for_a_fivem_security_application_call",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images_new/banner_banner_cover_for_a_fivem_security_application_call.jpg"
+    },
+    ["car"] = {
+        label = "car",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images_new/car.png"
+    },
+    ["d7a740b1-915d-403d-afcd-fa63edbf22ab"] = {
+        label = "d7a740b1-915d-403d-afcd-fa63edbf22ab",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images_new/d7a740b1-915d-403d-afcd-fa63edbf22ab.png"
+    },
+    ["edited_banner_a_cyberpunk_figure_wearing_a_neon_outlined_goat_ma"] = {
+        label = "edited_banner_a_cyberpunk_figure_wearing_a_neon_outlined_goat_ma",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images_new/edited_banner_a_cyberpunk_figure_wearing_a_neon_outlined_goat_ma.png"
+    },
+    ["edited_banner_a_developer_wearing_a_vibrant_neon_goat_mask__cyan"] = {
+        label = "edited_banner_a_developer_wearing_a_vibrant_neon_goat_mask__cyan",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images_new/edited_banner_a_developer_wearing_a_vibrant_neon_goat_mask__cyan.png"
+    },
+    ["edited_banner_a_mysterious_figure_in_a_dark_hoodie_wearing_a_glo"] = {
+        label = "edited_banner_a_mysterious_figure_in_a_dark_hoodie_wearing_a_glo",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images_new/edited_banner_a_mysterious_figure_in_a_dark_hoodie_wearing_a_glo.png"
+    },
+    ["edited_banner_a_person_wearing_a_glowing_neon_goat_mask_in_cyan_"] = {
+        label = "edited_banner_a_person_wearing_a_glowing_neon_goat_mask_in_cyan_",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images_new/edited_banner_a_person_wearing_a_glowing_neon_goat_mask_in_cyan_.png"
+    },
+    ["edited_banner_a_vibrant__abstract_tech_banner_with_a_dark_backgr"] = {
+        label = "edited_banner_a_vibrant__abstract_tech_banner_with_a_dark_backgr",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images_new/edited_banner_a_vibrant__abstract_tech_banner_with_a_dark_backgr.png"
+    },
+    ["edited_banner_a_visually_striking_banner_with_a_focus_on_hologra"] = {
+        label = "edited_banner_a_visually_striking_banner_with_a_focus_on_hologra",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images_new/edited_banner_a_visually_striking_banner_with_a_focus_on_hologra.png"
+    },
+    ["edited_banner_a_wide_shot_of_a_futuristic_office_desk_in_a_dark_ (1)"] = {
+        label = "edited_banner_a_wide_shot_of_a_futuristic_office_desk_in_a_dark_ (1)",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images_new/edited_banner_a_wide_shot_of_a_futuristic_office_desk_in_a_dark_ (1).png"
+    },
+    ["edited_banner_a_wide_shot_of_a_futuristic_office_desk_in_a_dark_ (2)"] = {
+        label = "edited_banner_a_wide_shot_of_a_futuristic_office_desk_in_a_dark_ (2)",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images_new/edited_banner_a_wide_shot_of_a_futuristic_office_desk_in_a_dark_ (2).png"
+    },
+    ["edited_banner_a_wide_shot_of_a_futuristic_office_desk_in_a_dark_ (3)"] = {
+        label = "edited_banner_a_wide_shot_of_a_futuristic_office_desk_in_a_dark_ (3)",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images_new/edited_banner_a_wide_shot_of_a_futuristic_office_desk_in_a_dark_ (3).png"
+    },
+    ["edited_banner_a_wide_shot_of_a_futuristic_office_desk_in_a_dark_"] = {
+        label = "edited_banner_a_wide_shot_of_a_futuristic_office_desk_in_a_dark_",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images_new/edited_banner_a_wide_shot_of_a_futuristic_office_desk_in_a_dark_.png"
+    },
+    ["edited_banner_banner_cover_for_a_fivem_security_application_call"] = {
+        label = "edited_banner_banner_cover_for_a_fivem_security_application_call",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images_new/edited_banner_banner_cover_for_a_fivem_security_application_call.png"
+    },
+    ["edited_banner_in_a_dark__high_tech_room__a_person_in_a_glowing_c"] = {
+        label = "edited_banner_in_a_dark__high_tech_room__a_person_in_a_glowing_c",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images_new/edited_banner_in_a_dark__high_tech_room__a_person_in_a_glowing_c.png"
+    },
+    ["edited_banner_in_a_dimly_lit__futuristic_hacker_s_den__a_person_"] = {
+        label = "edited_banner_in_a_dimly_lit__futuristic_hacker_s_den__a_person_",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images_new/edited_banner_in_a_dimly_lit__futuristic_hacker_s_den__a_person_.png"
+    },
+    ["edited_banner_manter_o_banner_original_de_estilo_retrowave_com_a"] = {
+        label = "edited_banner_manter_o_banner_original_de_estilo_retrowave_com_a",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images_new/edited_banner_manter_o_banner_original_de_estilo_retrowave_com_a.png"
+    },
+    ["edited_banner_style__cyberpunk__ui_focused__digital_art__main_sc"] = {
+        label = "edited_banner_style__cyberpunk__ui_focused__digital_art__main_sc",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images_new/edited_banner_style__cyberpunk__ui_focused__digital_art__main_sc.png"
+    },
+    ["edited_banner_style__gritty__hyper_realistic_digital_painting_co"] = {
+        label = "edited_banner_style__gritty__hyper_realistic_digital_painting_co",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images_new/edited_banner_style__gritty__hyper_realistic_digital_painting_co.png"
+    },
+    ["edited_banner_the_developer_s_hologram__style__minimalist__clean"] = {
+        label = "edited_banner_the_developer_s_hologram__style__minimalist__clean",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images_new/edited_banner_the_developer_s_hologram__style__minimalist__clean.png"
+    },
+    ["edited_banner_uploaded_imagethe_developer_s_hologram__style__min (1)"] = {
+        label = "edited_banner_uploaded_imagethe_developer_s_hologram__style__min (1)",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images_new/edited_banner_uploaded_imagethe_developer_s_hologram__style__min (1).png"
+    },
+    ["edited_banner_uploaded_imagethe_developer_s_hologram__style__min (2)"] = {
+        label = "edited_banner_uploaded_imagethe_developer_s_hologram__style__min (2)",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images_new/edited_banner_uploaded_imagethe_developer_s_hologram__style__min (2).png"
+    },
+    ["edited_banner_youtube_channel_art_for_a_gaming_channel_named__mi"] = {
+        label = "edited_banner_youtube_channel_art_for_a_gaming_channel_named__mi",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images_new/edited_banner_youtube_channel_art_for_a_gaming_channel_named__mi.png"
+    },
+    ["edited-image (1) (1)"] = {
+        label = "edited-image (1) (1)",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images_new/edited-image (1) (1).png"
+    },
+    ["edited-image (1)"] = {
+        label = "edited-image (1)",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images_new/edited-image (1).png"
+    },
+    ["edited-image (2) (1)"] = {
+        label = "edited-image (2) (1)",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images_new/edited-image (2) (1).png"
+    },
+    ["edited-image (3) (1)"] = {
+        label = "edited-image (3) (1)",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images_new/edited-image (3) (1).png"
+    },
+    ["edited-image (3)"] = {
+        label = "edited-image (3)",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images_new/edited-image (3).png"
+    },
+    ["edited-image (4) (1)"] = {
+        label = "edited-image (4) (1)",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images_new/edited-image (4) (1).png"
+    },
+    ["edited-image (4)"] = {
+        label = "edited-image (4)",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images_new/edited-image (4).png"
+    },
+    ["edited-image (5) (1)"] = {
+        label = "edited-image (5) (1)",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images_new/edited-image (5) (1).png"
+    },
+    ["edited-image (5)"] = {
+        label = "edited-image (5)",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images_new/edited-image (5).png"
+    },
+    ["edited-image (6) (1)"] = {
+        label = "edited-image (6) (1)",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images_new/edited-image (6) (1).png"
+    },
+    ["edited-image (6)"] = {
+        label = "edited-image (6)",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images_new/edited-image (6).png"
+    },
+    ["edited-image (7)"] = {
+        label = "edited-image (7)",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images_new/edited-image (7).png"
+    },
+    ["edited-image"] = {
+        label = "edited-image",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images_new/edited-image.png"
+    },
+    ["fee2e07d-a6d0-4f93-b6b2-ecae7b331454"] = {
+        label = "fee2e07d-a6d0-4f93-b6b2-ecae7b331454",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images_new/fee2e07d-a6d0-4f93-b6b2-ecae7b331454.png"
+    },
+    ["file_00000000d6e471f59a2394d2db6017eb"] = {
+        label = "file_00000000d6e471f59a2394d2db6017eb",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images_new/file_00000000d6e471f59a2394d2db6017eb.png"
+    },
+    ["file_000000004eb47246a28c5227f0eb2809"] = {
+        label = "file_000000004eb47246a28c5227f0eb2809",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images_new/file_000000004eb47246a28c5227f0eb2809.png"
+    },
+    ["file_000000007134720eae0b559020b25e7d"] = {
+        label = "file_000000007134720eae0b559020b25e7d",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images_new/file_000000007134720eae0b559020b25e7d.png"
+    },
+    ["fivem"] = {
+        label = "fivem",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images_new/fivem.png"
+    },
+    ["Fivemanage_1764523041_app"] = {
+        label = "Fivemanage_1764523041_app",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images_new/Fivemanage_1764523041_app.png"
+    },
+    ["Fivemanage_file_000000002730720e858344c48c8c8106_app"] = {
+        label = "Fivemanage_file_000000002730720e858344c48c8c8106_app",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images_new/Fivemanage_file_000000002730720e858344c48c8c8106_app.png"
+    },
+    ["Fivemanage_IMG_20251130_145157_app"] = {
+        label = "Fivemanage_IMG_20251130_145157_app",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images_new/Fivemanage_IMG_20251130_145157_app.jpg"
+    },
+    ["Generated Image November 06, 2025 - 8_41PM"] = {
+        label = "Generated Image November 06, 2025 - 8_41PM",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images_new/Generated Image November 06, 2025 - 8_41PM.png"
+    },
+    ["Generated Image November 09, 2025 - 12_24AM (1)"] = {
+        label = "Generated Image November 09, 2025 - 12_24AM (1)",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images_new/Generated Image November 09, 2025 - 12_24AM (1).png"
+    },
+    ["Generated Image November 09, 2025 - 12_24AM"] = {
+        label = "Generated Image November 09, 2025 - 12_24AM",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images_new/Generated Image November 09, 2025 - 12_24AM.png"
+    },
+    ["Generated Image November 10, 2025 - 6_04PM"] = {
+        label = "Generated Image November 10, 2025 - 6_04PM",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images_new/Generated Image November 10, 2025 - 6_04PM.png"
+    },
+    ["Generated Image November 26, 2025 - 12_51PM"] = {
+        label = "Generated Image November 26, 2025 - 12_51PM",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images_new/Generated Image November 26, 2025 - 12_51PM.png"
+    },
+    ["Generated Image November 29, 2025 - 4_30PM"] = {
+        label = "Generated Image November 29, 2025 - 4_30PM",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images_new/Generated Image November 29, 2025 - 4_30PM.png"
+    },
+    ["git"] = {
+        label = "git",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images_new/git.png"
+    },
+    ["Google_AI_Studio_2025-10-11T15_36_14.983Z"] = {
+        label = "Google_AI_Studio_2025-10-11T15_36_14.983Z",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images_new/Google_AI_Studio_2025-10-11T15_36_14.983Z.png"
+    },
+    ["i4_1"] = {
+        label = "i4_1",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images_new/i4_1.png"
+    },
+    ["i4_2"] = {
+        label = "i4_2",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images_new/i4_2.png"
+    },
+    ["i4_3"] = {
+        label = "i4_3",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images_new/i4_3.png"
+    },
+    ["IMG_20251126_124519"] = {
+        label = "IMG_20251126_124519",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images_new/IMG_20251126_124519.jpg"
+    },
+    ["IMG_20251126_125311"] = {
+        label = "IMG_20251126_125311",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images_new/IMG_20251126_125311.png"
+    },
+    ["IMG_20251126_183925"] = {
+        label = "IMG_20251126_183925",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images_new/IMG_20251126_183925.jpg"
+    },
+    ["IMG_20251126_184155"] = {
+        label = "IMG_20251126_184155",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images_new/IMG_20251126_184155.jpg"
+    },
+    ["IMG_20251127_231412"] = {
+        label = "IMG_20251127_231412",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images_new/IMG_20251127_231412.png"
+    },
+    ["img1"] = {
+        label = "img1",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images_new/img1.jpg"
+    },
+    ["kq_tow_rope"] = {
+        label = "kq_tow_rope",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images_new/kq_tow_rope.png"
+    },
+    ["kq_winch"] = {
+        label = "kq_winch",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images_new/kq_winch.png"
+    },
+    ["missing"] = {
+        label = "missing",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images_new/missing.png"
+    },
+    ["mist"] = {
+        label = "mist",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images_new/mist.png"
+    },
+    ["mist_logo"] = {
+        label = "mist_logo",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images_new/mist_logo.png"
+    },
+    ["shared_image_1764160855369"] = {
+        label = "shared_image_1764160855369",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images_new/shared_image_1764160855369.jpg"
+    },
+    ["sprite_sheet"] = {
+        label = "sprite_sheet",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images_new/sprite_sheet.png"
+    },
+    ["v6_1"] = {
+        label = "v6_1",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images_new/v6_1.png"
+    },
+    ["v6_2"] = {
+        label = "v6_2",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images_new/v6_2.png"
+    },
+    ["v6_3"] = {
+        label = "v6_3",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images_new/v6_3.png"
+    },
+    ["v6_4"] = {
+        label = "v6_4",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images_new/v6_4.png"
+    },
+    ["v6_5"] = {
+        label = "v6_5",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images_new/v6_5.png"
+    },
+    ["v8_1"] = {
+        label = "v8_1",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images_new/v8_1.png"
+    },
+    ["v8_2"] = {
+        label = "v8_2",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images_new/v8_2.png"
+    },
+    ["v8_3"] = {
+        label = "v8_3",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images_new/v8_3.png"
+    },
+    ["v12_1"] = {
+        label = "v12_1",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images_new/v12_1.png"
+    },
+    ["v12_2"] = {
+        label = "v12_2",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images_new/v12_2.png"
+    }
+}
