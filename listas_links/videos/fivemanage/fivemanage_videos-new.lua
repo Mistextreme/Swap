acae9528acb16b88b1db15c53512b65ff7751669
Mist the Goat <62227356+Mistextreme@users.noreply.github.com>
2025-12-02 20:45:28 +0000

    Add files via upload

---
+++ listas_links/videos/fivemanage/fivemanage_videos-new.lua
@@ -0,0 +1,351 @@
+-- FiveManage Videos
+return {
+    ["grok_video_2025-12-01-11-20-18"] = {
+        label = "grok_video_2025-12-01-11-20-18",
+        video = "https://r2.fivemanage.com/video/U5fzIDsGTVpc.mp4"
+    },
+    ["grok_video_2025-12-01-11-23-45"] = {
+        label = "grok_video_2025-12-01-11-23-45",
+        video = "https://r2.fivemanage.com/video/TZ37SH9xiC42.mp4"
+    },
+    ["grok_video_2025-12-01-11-29-24"] = {
+        label = "grok_video_2025-12-01-11-29-24",
+        video = "https://r2.fivemanage.com/video/dB3BroL4VAsV.mp4"
+    },
+    ["grok_video_2025-12-01-11-30-33"] = {
+        label = "grok_video_2025-12-01-11-30-33",
+        video = "https://r2.fivemanage.com/video/76B8XIDFLVXK.mp4"
+    },
+    ["grok_video_2025-12-01-11-31-15"] = {
+        label = "grok_video_2025-12-01-11-31-15",
+        video = "https://r2.fivemanage.com/video/CUlTiY4YOVPM.mp4"
+    },
+    ["grok_video_2025-12-01-11-32-55"] = {
+        label = "grok_video_2025-12-01-11-32-55",
+        video = "https://r2.fivemanage.com/video/NoJr37QpptnI.mp4"
+    },
+    ["grok_video_2025-12-01-11-35-55"] = {
+        label = "grok_video_2025-12-01-11-35-55",
+        video = "https://r2.fivemanage.com/video/MTZwBrUlQxOR.mp4"
+    },
+    ["grok_video_2025-12-01-11-36-00"] = {
+        label = "grok_video_2025-12-01-11-36-00",
+        video = "https://r2.fivemanage.com/video/l2oaoV5rL7VQ.mp4"
+    },
+    ["grok_video_2025-12-01-11-42-35"] = {
+        label = "grok_video_2025-12-01-11-42-35",
+        video = "https://r2.fivemanage.com/video/QBZiWeMT9ajD.mp4"
+    },
+    ["grok_video_2025-12-01-11-43-10"] = {
+        label = "grok_video_2025-12-01-11-43-10",
+        video = "https://r2.fivemanage.com/video/AfARH4evfvY3.mp4"
+    },
+    ["grok_video_2025-12-01-12-12-24"] = {
+        label = "grok_video_2025-12-01-12-12-24",
+        video = "https://r2.fivemanage.com/video/Q1ow7VEaPbKD.mp4"
+    },
+    ["grok_video_2025-12-01-12-15-52"] = {
+        label = "grok_video_2025-12-01-12-15-52",
+        video = "https://r2.fivemanage.com/video/Pdxl7zzeDFuK.mp4"
+    },
+    ["grok_video_2025-12-01-12-16-07"] = {
+        label = "grok_video_2025-12-01-12-16-07",
+        video = "https://r2.fivemanage.com/video/qOKhnSZmWhHd.mp4"
+    },
+    ["grok_video_2025-12-01-12-16-17"] = {
+        label = "grok_video_2025-12-01-12-16-17",
+        video = "https://r2.fivemanage.com/video/CKrFYG5DgFMm.mp4"
+    },
+    ["grok_video_2025-12-01-12-18-21"] = {
+        label = "grok_video_2025-12-01-12-18-21",
+        video = "https://r2.fivemanage.com/video/OzYh7MWypi6r.mp4"
+    },
+    ["grok_video_2025-12-01-12-18-31"] = {
+        label = "grok_video_2025-12-01-12-18-31",
+        video = "https://r2.fivemanage.com/video/40JJOXTfiJUZ.mp4"
+    },
+    ["grok_video_2025-12-01-12-22-05"] = {
+        label = "grok_video_2025-12-01-12-22-05",
+        video = "https://r2.fivemanage.com/video/LXv7TlD8j6dX.mp4"
+    },
+    ["grok_video_2025-12-01-12-24-10"] = {
+        label = "grok_video_2025-12-01-12-24-10",
+        video = "https://r2.fivemanage.com/video/OQ2PgYqPVz7N.mp4"
+    },
+    ["grok_video_2025-12-01-12-24-14"] = {
+        label = "grok_video_2025-12-01-12-24-14",
+        video = "https://r2.fivemanage.com/video/cCv8zRIAS21s.mp4"
+    },
+    ["grok_video_2025-12-01-12-24-19"] = {
+        label = "grok_video_2025-12-01-12-24-19",
+        video = "https://r2.fivemanage.com/video/Gd9Z4nqqinK7.mp4"
+    },
+    ["grok_video_2025-12-01-12-24-24"] = {
+        label = "grok_video_2025-12-01-12-24-24",
+        video = "https://r2.fivemanage.com/video/4BJyul83dfAf.mp4"
+    },
+    ["grok_video_2025-12-01-12-24-29"] = {
+        label = "grok_video_2025-12-01-12-24-29",
+        video = "https://r2.fivemanage.com/video/4MwlsRjzl3y0.mp4"
+    },
+    ["grok_video_2025-12-01-12-24-43"] = {
+        label = "grok_video_2025-12-01-12-24-43",
+        video = "https://r2.fivemanage.com/video/LDW5jztHf48X.mp4"
+    },
+    ["grok_video_2025-12-01-12-25-19"] = {
+        label = "grok_video_2025-12-01-12-25-19",
+        video = "https://r2.fivemanage.com/video/eNAXHzZ3Lypd.mp4"
+    },
+    ["grok_video_2025-12-01-12-25-29"] = {
+        label = "grok_video_2025-12-01-12-25-29",
+        video = "https://r2.fivemanage.com/video/SMAI34DwWZiw.mp4"
+    },
+    ["grok_video_2025-12-01-12-25-37"] = {
+        label = "grok_video_2025-12-01-12-25-37",
+        video = "https://r2.fivemanage.com/video/FopmqtoheBQS.mp4"
+    },
+    ["grok_video_2025-12-01-12-25-43"] = {
+        label = "grok_video_2025-12-01-12-25-43",
+        video = "https://r2.fivemanage.com/video/ZESd8KJKnOSu.mp4"
+    },
+    ["grok_video_2025-12-01-12-25-51"] = {
+        label = "grok_video_2025-12-01-12-25-51",
+        video = "https://r2.fivemanage.com/video/DkmBImhjHUxk.mp4"
+    },
+    ["grok_video_2025-12-01-12-25-56"] = {
+        label = "grok_video_2025-12-01-12-25-56",
+        video = "https://r2.fivemanage.com/video/wScjjfG4vau0.mp4"
+    },
+    ["grok_video_2025-12-01-12-31-30"] = {
+        label = "grok_video_2025-12-01-12-31-30",
+        video = "https://r2.fivemanage.com/video/TKk8CGoAlcg5.mp4"
+    },
+    ["grok_video_2025-12-01-12-31-41"] = {
+        label = "grok_video_2025-12-01-12-31-41",
+        video = "https://r2.fivemanage.com/video/l2fmwVFPuSIs.mp4"
+    },
+    ["grok_video_2025-12-01-12-32-26"] = {
+        label = "grok_video_2025-12-01-12-32-26",
+        video = "https://r2.fivemanage.com/video/8WP4ZkvmOo0s.mp4"
+    },
+    ["grok_video_2025-12-01-12-33-27"] = {
+        label = "grok_video_2025-12-01-12-33-27",
+        video = "https://r2.fivemanage.com/video/CqelQVxNjo2g.mp4"
+    },
+    ["grok_video_2025-12-01-12-34-41"] = {
+        label = "grok_video_2025-12-01-12-34-41",
+        video = "https://r2.fivemanage.com/video/0SWQkfFLwfXt.mp4"
+    },
+    ["mist0"] = {
+        label = "mist0",
+        video = "https://r2.fivemanage.com/video/sTJCopAvY6OC.mp4"
+    },
+    ["1caf9d5b-699a-4ab1-a067-1b2449d21158"] = {
+        label = "1caf9d5b-699a-4ab1-a067-1b2449d21158",
+        video = "https://r2.fivemanage.com/video/GgoD6eiKNCcU.mp4"
+    },
+    ["2ff9c015-3fc2-4ef9-94ba-81c4964b609c"] = {
+        label = "2ff9c015-3fc2-4ef9-94ba-81c4964b609c",
+        video = "https://r2.fivemanage.com/video/eQRccmqwhB6H.mp4"
+    },
+    ["6dda05a5-314e-4cb0-862a-c81e0046829f"] = {
+        label = "6dda05a5-314e-4cb0-862a-c81e0046829f",
+        video = "https://r2.fivemanage.com/video/dAMB2hWLmeuK.mp4"
+    },
+    ["14b594c1-a5c4-44ef-90aa-9bbb34a79336"] = {
+        label = "14b594c1-a5c4-44ef-90aa-9bbb34a79336",
+        video = "https://r2.fivemanage.com/video/C5Yj32g9DbjG.mp4"
+    },
+    ["48a67233-7c1c-4c0f-bf38-74fe7be0dce5"] = {
+        label = "48a67233-7c1c-4c0f-bf38-74fe7be0dce5",
+        video = "https://r2.fivemanage.com/video/nGagqBVpEA6k.mp4"
+    },
+    ["77fd0b80-bcff-11f0-9239-e54eb8f772e3 (1)"] = {
+        label = "77fd0b80-bcff-11f0-9239-e54eb8f772e3 (1)",
+        video = "https://r2.fivemanage.com/video/XSFw95MKXrhG.mp4"
+    },
+    ["77fd0b80-bcff-11f0-9239-e54eb8f772e3"] = {
+        label = "77fd0b80-bcff-11f0-9239-e54eb8f772e3",
+        video = "https://r2.fivemanage.com/video/8NroCsM0aeUy.mp4"
+    },
+    ["78a5faff-cb31-4cbf-a9bc-b2aa200884c2"] = {
+        label = "78a5faff-cb31-4cbf-a9bc-b2aa200884c2",
+        video = "https://r2.fivemanage.com/video/MU6nQyn85Ims.mp4"
+    },
+    ["084e7e80-be6d-11f0-abe4-e72863fad937"] = {
+        label = "084e7e80-be6d-11f0-abe4-e72863fad937",
+        video = "https://r2.fivemanage.com/video/WxxZ4Hknjsxg.mp4"
+    },
+    ["875645f0-ae6a-420f-91d8-abfe2152d366"] = {
+        label = "875645f0-ae6a-420f-91d8-abfe2152d366",
+        video = "https://r2.fivemanage.com/video/erZKbMmck3xs.mp4"
+    },
+    ["a1a57b1a-4f60-47e5-a553-a663b9410bbf"] = {
+        label = "a1a57b1a-4f60-47e5-a553-a663b9410bbf",
+        video = "https://r2.fivemanage.com/video/2dkqudrhhZ2D.mp4"
+    },
+    ["a1690a90-668a-4b5e-84fb-83839dff6ff0"] = {
+        label = "a1690a90-668a-4b5e-84fb-83839dff6ff0",
+        video = "https://r2.fivemanage.com/video/EfcQ35Q9n1Dc.mp4"
+    },
+    ["analizer_bot (1)"] = {
+        label = "analizer_bot (1)",
+        video = "https://r2.fivemanage.com/video/g1iWY6J4Ft2z.mp4"
+    },
+    ["analizer_bot"] = {
+        label = "analizer_bot",
+        video = "https://r2.fivemanage.com/video/DlHsfaGlNEaZ.mp4"
+    },
+    ["bff8134d-354d-4fe2-ba8f-415f04636ee5 (1)"] = {
+        label = "bff8134d-354d-4fe2-ba8f-415f04636ee5 (1)",
+        video = "https://r2.fivemanage.com/video/S8FqjlP2aiYh.mp4"
+    },
+    ["bff8134d-354d-4fe2-ba8f-415f04636ee5"] = {
+        label = "bff8134d-354d-4fe2-ba8f-415f04636ee5",
+        video = "https://r2.fivemanage.com/video/3EMIH4uaoceg.mp4"
+    },
+    ["Code_intro (1)"] = {
+        label = "Code_intro (1)",
+        video = "https://r2.fivemanage.com/video/znWzNxPhdb4l.mp4"
+    },
+    ["Code_intro (2)"] = {
+        label = "Code_intro (2)",
+        video = "https://r2.fivemanage.com/video/WY4KanF6R5Os.mp4"
+    },
+    ["coding_intro (1)"] = {
+        label = "coding_intro (1)",
+        video = "https://r2.fivemanage.com/video/CTYP8rfiedrZ.mp4"
+    },
+    ["coding_intro"] = {
+        label = "coding_intro",
+        video = "https://r2.fivemanage.com/video/P7zPjodyNeH3.mp4"
+    },
+    ["fivemanage_tool"] = {
+        label = "fivemanage_tool",
+        video = "https://r2.fivemanage.com/video/58TnQXasxd5X.mp4"
+    },
+    ["fivemanage_tool_0"] = {
+        label = "fivemanage_tool_0",
+        video = "https://r2.fivemanage.com/video/AAp6AmFdCLYV.mp4"
+    },
+    ["fivemanage_tool_1"] = {
+        label = "fivemanage_tool_1",
+        video = "https://r2.fivemanage.com/video/9xhIaJpgpTNo.mp4"
+    },
+    ["fivemanage_tool_2"] = {
+        label = "fivemanage_tool_2",
+        video = "https://r2.fivemanage.com/video/OTBDOlizPeR9.mp4"
+    },
+    ["fivemanage_tool_3"] = {
+        label = "fivemanage_tool_3",
+        video = "https://r2.fivemanage.com/video/pTIDPvcJPvcj.mp4"
+    },
+    ["fivemanage_tool_4"] = {
+        label = "fivemanage_tool_4",
+        video = "https://r2.fivemanage.com/video/hWBE5xYOc7an.mp4"
+    },
+    ["framework_to-esx"] = {
+        label = "framework_to-esx",
+        video = "https://r2.fivemanage.com/video/RgnYy6EUzdUj.mp4"
+    },
+    ["FrameworksConvert"] = {
+        label = "FrameworksConvert",
+        video = "https://r2.fivemanage.com/video/UhiuSXhq3xsi.mp4"
+    },
+    ["FrameworksConvert0"] = {
+        label = "FrameworksConvert0",
+        video = "https://r2.fivemanage.com/video/8Hwg6rB1Y9T9.mp4"
+    },
+    ["Generated File November 06, 2025 - 6_46PM"] = {
+        label = "Generated File November 06, 2025 - 6_46PM",
+        video = "https://r2.fivemanage.com/video/Z872oZ6hhDyZ.mp4"
+    },
+    ["Generated File November 06, 2025 - 8_44PM"] = {
+        label = "Generated File November 06, 2025 - 8_44PM",
+        video = "https://r2.fivemanage.com/video/tz2WdBW0jok7.mp4"
+    },
+    ["Generated File November 06, 2025 - 8_46PM"] = {
+        label = "Generated File November 06, 2025 - 8_46PM",
+        video = "https://r2.fivemanage.com/video/BOOuSc2u9Du9.mp4"
+    },
+    ["Generated File November 06, 2025 - 8_52PM"] = {
+        label = "Generated File November 06, 2025 - 8_52PM",
+        video = "https://r2.fivemanage.com/video/JOKay4SCH8op.mp4"
+    },
+    ["Generated File November 10, 2025 - 6_13PM"] = {
+        label = "Generated File November 10, 2025 - 6_13PM",
+        video = "https://r2.fivemanage.com/video/JLST8071UFye.mp4"
+    },
+    ["Generated video 1"] = {
+        label = "Generated video 1",
+        video = "https://r2.fivemanage.com/video/BglhxEZsIbLj.mp4"
+    },
+    ["goat_coding (1)"] = {
+        label = "goat_coding (1)",
+        video = "https://r2.fivemanage.com/video/TRIsCcbM8P8V.mp4"
+    },
+    ["goat_coding"] = {
+        label = "goat_coding",
+        video = "https://r2.fivemanage.com/video/phrltlX5zha7.mp4"
+    },
+    ["LogoIntro"] = {
+        label = "LogoIntro",
+        video = "https://r2.fivemanage.com/video/8HIYsDdAwt0c.mp4"
+    },
+    ["Mist__dev"] = {
+        label = "Mist__dev",
+        video = "https://r2.fivemanage.com/video/J8wZmMiNiOyJ.mp4"
+    },
+    ["mist_logo"] = {
+        label = "mist_logo",
+        video = "https://r2.fivemanage.com/video/LKuO4BR1yZla.mp4"
+    },
+    ["MistDev"] = {
+        label = "MistDev",
+        video = "https://r2.fivemanage.com/video/66YGI1Cs7lip.mp4"
+    },
+    ["Mist--devv"] = {
+        label = "Mist--devv",
+        video = "https://r2.fivemanage.com/video/XtJp9qNAmMyj.mp4"
+    },
+    ["MistGoat"] = {
+        label = "MistGoat",
+        video = "https://r2.fivemanage.com/video/TUUmVqMCgE1O.mp4"
+    },
+    ["MistGoat0"] = {
+        label = "MistGoat0",
+        video = "https://r2.fivemanage.com/video/1R78MUpMzcNA.mp4"
+    },
+    ["MistGoat1"] = {
+        label = "MistGoat1",
+        video = "https://r2.fivemanage.com/video/MWKPSR37aofG.mp4"
+    },
+    ["MistIntro"] = {
+        label = "MistIntro",
+        video = "https://r2.fivemanage.com/video/cTit9XTymmTW.mp4"
+    },
+    ["mistlogo"] = {
+        label = "mistlogo",
+        video = "https://r2.fivemanage.com/video/VaH4d3EcLuVt.mp4"
+    },
+    ["MistTranslator"] = {
+        label = "MistTranslator",
+        video = "https://r2.fivemanage.com/video/N9TrVwCy7OsN.mp4"
+    },
+    ["motion2Fast_Animate_the_holographic_Git_workflow_projection_to_0"] = {
+        label = "motion2Fast_Animate_the_holographic_Git_workflow_projection_to_0",
+        video = "https://r2.fivemanage.com/video/GmUS7ejryoXU.mp4"
+    },
+    ["motion2Fast_Create_an_epic_and_short_introduction_5_seconds_fo_0"] = {
+        label = "motion2Fast_Create_an_epic_and_short_introduction_5_seconds_fo_0",
+        video = "https://r2.fivemanage.com/video/BPXwyfDKmjzw.mp4"
+    },
+    ["motion2Fast_Create_an_epic_and_short_introduction_35_seconds_f_0"] = {
+        label = "motion2Fast_Create_an_epic_and_short_introduction_35_seconds_f_0",
+        video = "https://r2.fivemanage.com/video/mNaLr05x8Mwv.mp4"
+    },
+    ["motion2Fast_Crie_uma_introduo_pica_e_muito_curta_35_segundos_n_0"] = {
+        label = "motion2Fast_Crie_uma_introduo_pica_e_muito_curta_35_segundos_n_0",
+        video = "https://r2.fivemanage.com/video/kpaygEHWjPi5.mp4"
+    }
+}
