1fcb0fd9679b17a915c75ea0645db36d140f5b70
Mist the Goat <62227356+Mistextreme@users.noreply.github.com>
2025-12-05 22:38:39 +0000

    Upload gifs.lua

---
+++ links/images/fivemanage/gifs.lua
@@ -0,0 +1,27 @@
+-- FiveManage Images
+return {
+    ["mist_gifs_17"] = {
+        label = "mist_gifs_17",
+        image = "https://r2.fivemanage.com/image/HLotlAxPdymS.gif"
+    },
+    ["mist_gifs_16"] = {
+        label = "mist_gifs_16",
+        image = "https://r2.fivemanage.com/image/e8FY4ZQyixIW.gif"
+    },
+    ["mist_gifs_15"] = {
+        label = "mist_gifs_15",
+        image = "https://r2.fivemanage.com/image/wyVAaIcMvUsj.gif"
+    },
+    ["mist_gifs_14"] = {
+        label = "mist_gifs_14",
+        image = "https://r2.fivemanage.com/image/vSnjPY1VU634.gif"
+    },
+    ["mist_gifs_12"] = {
+        label = "mist_gifs_12",
+        image = "https://r2.fivemanage.com/image/x9FrzsU9NhJq.gif"
+    },
+    ["mist_gifs_13"] = {
+        label = "mist_gifs_13",
+        image = "https://r2.fivemanage.com/image/bpw9wnwzOaQV.gif"
+    }
+}
