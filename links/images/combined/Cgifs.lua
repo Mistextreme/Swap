1eb96d71ca1aa7ba77ce5a30336ec1a69aec0df5
Mist the Goat <62227356+Mistextreme@users.noreply.github.com>
2025-12-05 22:39:14 +0000

    Upload Cgifs.lua

---
+++ links/images/combined/Cgifs.lua
@@ -0,0 +1,63 @@
+-- Combined Images (FiveManage + GitHub)
+return {
+    ["mist_gifs_17"] = {
+        label = "mist_gifs_17",
+        image = "https://r2.fivemanage.com/image/HLotlAxPdymS.gif",
+        source = "fivemanage"
+    },
+    ["mist_gifs_16"] = {
+        label = "mist_gifs_16",
+        image = "https://r2.fivemanage.com/image/e8FY4ZQyixIW.gif",
+        source = "fivemanage"
+    },
+    ["mist_gifs_15"] = {
+        label = "mist_gifs_15",
+        image = "https://r2.fivemanage.com/image/wyVAaIcMvUsj.gif",
+        source = "fivemanage"
+    },
+    ["mist_gifs_14"] = {
+        label = "mist_gifs_14",
+        image = "https://r2.fivemanage.com/image/vSnjPY1VU634.gif",
+        source = "fivemanage"
+    },
+    ["mist_gifs_12"] = {
+        label = "mist_gifs_12",
+        image = "https://r2.fivemanage.com/image/x9FrzsU9NhJq.gif",
+        source = "fivemanage"
+    },
+    ["mist_gifs_13"] = {
+        label = "mist_gifs_13",
+        image = "https://r2.fivemanage.com/image/bpw9wnwzOaQV.gif",
+        source = "fivemanage"
+    },
+    ["mist_gifs_17"] = {
+        label = "mist_gifs_17",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images/mist_gifs_17.gif",
+        source = "github"
+    },
+    ["mist_gifs_16"] = {
+        label = "mist_gifs_16",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images/mist_gifs_16.gif",
+        source = "github"
+    },
+    ["mist_gifs_15"] = {
+        label = "mist_gifs_15",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images/mist_gifs_15.gif",
+        source = "github"
+    },
+    ["mist_gifs_14"] = {
+        label = "mist_gifs_14",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images/mist_gifs_14.gif",
+        source = "github"
+    },
+    ["mist_gifs_12"] = {
+        label = "mist_gifs_12",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images/mist_gifs_12.gif",
+        source = "github"
+    },
+    ["mist_gifs_13"] = {
+        label = "mist_gifs_13",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images/mist_gifs_13.gif",
+        source = "github"
+    }
+}
