0b4cdde26d125da8300fa98e9cdb7248eb07ef82
Mist the Goat <62227356+Mistextreme@users.noreply.github.com>
2025-12-05 22:38:49 +0000

    Upload gifs.lua

---
+++ links/images/github/gifs.lua
@@ -0,0 +1,27 @@
+-- GitHub Images
+return {
+    ["mist_gifs_17"] = {
+        label = "mist_gifs_17",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images/mist_gifs_17.gif"
+    },
+    ["mist_gifs_16"] = {
+        label = "mist_gifs_16",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images/mist_gifs_16.gif"
+    },
+    ["mist_gifs_15"] = {
+        label = "mist_gifs_15",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images/mist_gifs_15.gif"
+    },
+    ["mist_gifs_14"] = {
+        label = "mist_gifs_14",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images/mist_gifs_14.gif"
+    },
+    ["mist_gifs_12"] = {
+        label = "mist_gifs_12",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images/mist_gifs_12.gif"
+    },
+    ["mist_gifs_13"] = {
+        label = "mist_gifs_13",
+        image = "https://raw.githubusercontent.com/Mistextreme/Swap/main/images/mist_gifs_13.gif"
+    }
+}
